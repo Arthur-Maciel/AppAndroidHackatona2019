f011511bafd5b9a647e9b68e1a8511e0f91c9b2b
F4NT0 <gabriel.stundner@acad.pucrs.br>
2019-09-14 17:56:47 -0300

    Criacao de circulos feita

---
+++ app/src/main/java/br/com/southsystem/hackatona2019/Area.kt
@@ -0,0 +1,8 @@
+package br.com.southsystem.hackatona2019
+
+data class Area(
+    val latitude : String,
+    val longitude : String,
+    val raio : String,
+
+)
--- app/src/main/java/br/com/southsystem/hackatona2019/MapsActivity.kt
+++ app/src/main/java/br/com/southsystem/hackatona2019/MapsActivity.kt
@@ -1,5 +1,6 @@
 package br.com.southsystem.hackatona2019
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,12 +8,16 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.CircleOptions
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import kotlin.collections.ArrayList
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
+
+    val areas: ArrayList<Any>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,23 +26,88 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        popula()
+
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+    fun popula() {
+//        for (i in list) {
+//            if (i.selecionada!!) {
+//                dados!!.add(i.name)
+//            }
+//        }
+
+
+        val Area1 = object {
+            val latitude = "-30.0699753"
+            val longitude = "-51.1757777"
+            val raio = "500"
+            val status = object {
+                val situacao = "em alerta"
+            }
+        }
+        val Area2 = object {
+            val latitude = "-30.0562199"
+            val longitude = "-51.1800445"
+            val raio = "300"
+            val status = object {
+                val situacao = "sereno"
+            }
+        }
+        val Area3 = object {
+            val latitude = "-51.1800445"
+            val longitude = "-51.166284"
+            val raio = "150"
+            val status = object {
+                val situacao = "perigo"
+            }
+        }
+
+        areas?.add(Area1)
+        areas?.add(Area2)
+        areas?.add(Area3)
+
+    }
+
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        // Add a marker in PUCRS and move the camera
+        val puc = LatLng(-30.0592363, -51.1751851)
+        mMap.addMarker(MarkerOptions().position(puc).title("Marker in PUCRS"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(puc))
+        mMap.setMinZoomPreference(15f)
+
+        loadAreas()
+
+        mMap.setOnCircleClickListener {
+            println(it.isVisible)
+        }
     }
+
+
+    private fun loadAreas() {
+
+        if (areas != null) {
+            for (i in areas) {
+                
+            }
+        }
+
+
+
+        val redArea2 = LatLng(-30.0699753, -51.1757777)
+
+       mMap.addCircle(
+            CircleOptions()
+                .center(redArea2)
+                .radius(500.0)
+                .strokeWidth(5f)
+                .strokeColor(Color.RED)
+                .fillColor(Color.argb(70,150,70,70))
+                .clickable(true)
+        )
+    }
+
 }
