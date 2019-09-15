a91e7ba4883e0272e4b3c2b490ebcd0ee12229c7
F4NT0 <gabriel.stundner@acad.pucrs.br>
2019-09-15 03:41:35 -0300

    Mapa com botao v1

--- app/src/main/java/br/com/southsystem/hackatona2019/MainActivity.kt
+++ app/src/main/java/br/com/southsystem/hackatona2019/MainActivity.kt
@@ -5,16 +5,37 @@ import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        addFragment(MapFragment(), false, "one")
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
 
+//        addFragment(MapFragment(), false, "one")
+
+    }
+
+    override fun onMapReady(p0: GoogleMap) {
+        mMap = p0
+
+        val puc = LatLng(-30.0592363, -51.1751851)
+        mMap.addMarker(MarkerOptions().position(puc).title("Marker in PUCRS"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(puc))
+        mMap.setMinZoomPreference(15f)
     }
 
     fun addFragment(fragment: Fragment, addToBackStack: Boolean, tag: String) {
